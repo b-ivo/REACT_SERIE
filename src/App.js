44e0e79a0669f4245d428be435917b3df22311b5
b-ivo <ivobryan12@gmail.com>
2025-05-14 18:54:29 +0200

    app.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = "IVO";
+  const handleNameChange = () =>{
+    const name = ['Ivo','Herve','Kenny','king'];
+    const int = Math.floor(Math.random() * 3);
+    return name[int]
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to seethe changes.
+          Hello { handleNameChange() }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <p>{ name }</p> */}
+
       </header>
     </div>
   );
